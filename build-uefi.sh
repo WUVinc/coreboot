bf53d099b2ade20b569fe39c3bd93d8fca86ce04
Matt DeVillier <matt.devillier@gmail.com>
2023-01-24 12:41:09 -0600

    Update version string, build script, drop non-CroS cfgs
    Update version/branding, strip json creation
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-uefi.sh
+++ build-uefi.sh
@@ -7,10 +7,6 @@ platforms=('snb_ivb' 'hsw' 'byt' 'bdw' 'bsw' 'skl' 'apl' 'kbl' 'whl' 'glk' \
            'cml' 'jsl' 'tgl' 'str' 'zen2' 'adl')
 build_targets=()
 
-json_file=cbmodels.json
-rom_path=https://www.mrchromebox.tech/files/firmware/full_rom/
-echo -e "{" > $json_file
-
 if [ -z "$1" ]; then
 	for subdir in "${platforms[@]}"; do
 		for cfg in configs/$subdir/config*.*; do
@@ -25,7 +21,7 @@ fi
 rev=$(git describe --tags --dirty)
 
 for device in "${build_targets[@]}"; do
-	filename="coreboot_tiano-${device}-mrchromebox_$(date +"%Y%m%d").rom"
+	filename="coreboot_tiano-${device}-WUV.MrC_$(date +"%Y%m%d").rom"
 	rm -f ~/dev/firmware/${filename}*
 	rm -rf ./build
 	cfg_file=$(find ./configs -name "config.$device.uefi")
@@ -36,12 +32,4 @@ for device in "${build_targets[@]}"; do
 	make -j$(nproc)
 	cp ./build/coreboot.rom ./${filename}
 	sha1sum ${filename} > ${filename}.sha1
-	echo -e "\t\"${device}\": {" >> $json_file
-	echo -e "\t\t\"url\": \"${rom_path}${filename}\"," >> $json_file
-	echo -e "\t\t\"sha1\": \"$(cat ${filename}.sha1 | awk 'NR==1{print $1}')\"" >> $json_file
-	echo -e "\t}," >> $json_file
-	mv ${filename}* ~/dev/firmware/
 done
-echo -e "}" >> $json_file
-# remove last comma
-sed -i 's/\(.*\),/\1/' $json_file
